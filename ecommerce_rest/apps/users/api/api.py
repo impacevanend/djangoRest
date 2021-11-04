9f18c5f32bff3119d11235fbbc636161a43e0135
impac <impacevanend@gmail.com>
2021-11-04 18:13:31 -0500

    se agregan decoradores al serializador

--- ecommerce_rest/apps/users/api/api.py
+++ ecommerce_rest/apps/users/api/api.py
@@ -1,12 +1,29 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
+#*Clase
+# class UserAPIview(APIView):
+#     #REcive la petición get
+#     def get(self, request):
+#         users = User.objects.all()
 
-class UserAPIview(APIView):
-    #REcive la petición get
-    def get(self, request):
+#         #*pasando la consulta a un serializador
+
+#         users_serializer = UserSerializer(users, many = True)
+#         #*Información enviada en data
+#         return Response(users_serializer.data)
+
+
+
+
+#Todo decorador
+@api_view(['GET','POST'])#*metodos [get, post, put, delete]
+def user_api_view(request):
+    
+    if request.method == 'GET':
         users = User.objects.all()
 
         #*pasando la consulta a un serializador
@@ -14,3 +31,14 @@ class UserAPIview(APIView):
         users_serializer = UserSerializer(users, many = True)
         #*Información enviada en data
         return Response(users_serializer.data)
+
+    elif request.method == 'POST':
+        users_serializer = UserSerializer(data = request.data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            """
+            Guarda la información serlializada cuando se envia una serie de parametros para GET
+            Y la información de una creación serializada
+            """
+            return Response(users_serializer.data)
+        return Response(users_serializer.errors)
--- ecommerce_rest/apps/users/api/urls.py
+++ ecommerce_rest/apps/users/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from apps.users.api.api import UserAPIview
+#from apps.users.api.api import UserAPIview # *vista sin decorardo
+from apps.users.api.api import user_api_view # *vista con decorador
+
 
 urlpatterns = [
-    path('usuario/', UserAPIview.as_view(), name = 'usuario_api'),
+    path('usuario/', user_api_view, name = 'usuario_api'),
 ]
